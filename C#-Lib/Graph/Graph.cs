e5fc9c2daa078cbf4f32e1ca58f45cfad574a8f3
Oliver Marie <oliverjamarie@gmail.com>
2021-09-28 22:56:29 +0200

    Update Graph.cs
    Added support to build a graph based off adjacency list

--- C#-Lib/Graph/Graph.cs
+++ C#-Lib/Graph/Graph.cs
@@ -8,10 +8,76 @@ namespace Library.Graph
     {
 
         List<Node> nodes;
+        public bool allowSelfConnect { get; set; }
 
         public Graph()
         {
             nodes = new List<Node>();
+            allowSelfConnect = false;
+        }
+
+        public Graph(bool allowSelfConnect)
+        {
+            this.allowSelfConnect = allowSelfConnect;
+            nodes = new List<Node>();
+        }
+
+        public Graph(List<T> list)
+        {
+            nodes = new List<Node>();
+            
+            foreach(T i in list)
+            {
+                insert(i);
+            }
+        }
+
+        // used to create a graph based off an adjacency list
+        public Graph(Dictionary<T,List<T>> adjList)
+        {
+            nodes = new List<Node>();
+
+            foreach(KeyValuePair<T, List<T>> pair in adjList)
+            {
+                if (existsInGraph(pair.Key) == false)
+                {
+                    insert(pair.Key);
+                }
+
+                foreach(T t in pair.Value)
+                {
+                    if (existsInGraph(t) == false)
+                    {
+                        insert(t);
+                    }
+
+                    connectNodes(pair.Key, t, 1);
+                }
+            }
+        }
+
+        // used to create a graph based off an adjacency list
+        // (includes weights)
+        public Graph(Dictionary<T, Dictionary<T,double>> adjList)
+        {
+            nodes = new List<Node>();
+
+            foreach (KeyValuePair<T, Dictionary<T, double>> pair in adjList){
+                if (existsInGraph(pair.Key) == false)
+                {
+                    insert(pair.Key);
+                }
+
+                foreach(KeyValuePair<T, double> pair2 in pair.Value)
+                {
+                    if (existsInGraph(pair2.Key) == false)
+                    {
+                        insert(pair2.Key);
+                    }
+
+                    connectNodes(pair.Key, pair2.Key, pair2.Value);
+                }
+            }
         }
 
         private bool existsInGraph(T data)
@@ -65,8 +131,33 @@ namespace Library.Graph
             return false;
         }
 
+        public bool connectNodes(T start, Dictionary<T,double> targets)
+        {
+            foreach(KeyValuePair<T,double> pair in targets) {
+                if (!connectNodes(start, pair.Key, pair.Value))
+                {
+                    return false;
+                }
+            }
 
-        protected bool connectNodes(Node start, Node end, double cost)
+            return true;
+        }
+
+        public bool connectNodes(T start, List<T> targets)
+        {
+            foreach(T t in targets)
+            {
+                if (!connectNodes(start, t, 1))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        private bool connectNodes(Node start, Node end, double cost)
         {
             if ((nodes.Contains(start) && nodes.Contains(end)) == false)
                 return false;
