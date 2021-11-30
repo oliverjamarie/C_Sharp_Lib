328a72329dea3a852e394a965e7c9b059fb9585e
Oliver Marie <oliver.j.a.marie@gmail.com>
2021-11-30 17:41:53 +0100

    Tree Implementation

---
+++ Library/Tree/INode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C_Sharp_Lib.Library.Tree
+{
+    public interface INode<T> where T : IComparable
+    {
+        bool addChild(INode<T>child);
+        bool addChild(T data);
+        void addChildren(List<INode<T>> chidlrenIN);
+        bool Equals(object obj);
+        bool Equals(INode<T>other);
+        List<INode<T>> getChildren();
+        T getData();
+        INode<T>getParent();
+        List<INode<T>> getParents();
+        void setParent(INode<T> parent);
+        int getID();
+    }
+}
---
+++ Library/Tree/Tree.Node.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace C_Sharp_Lib.Library.Tree
+{
+    public partial class Tree<T>
+    {
+        protected class Node : INode<T>
+        {
+            List<INode<T>> children;
+            T data;
+            INode<T> parent;
+            protected static int countNodes = 0;
+            int id;
+
+            public Node()
+            {
+                children = new List<INode<T>>();
+                data = default(T);
+                parent = null;
+                id = countNodes++;
+            }
+
+            public Node(T data) : this()
+            {
+                this.data = data;
+            }
+
+            public Node(T data, List<Node> children) : this(data)
+            {
+                this.children.AddRange(children);
+            }
+
+            public Node(T data, Node parent) : this(data)
+            {
+                this.parent = parent;
+            }
+
+            public List<INode<T>> getChildren()
+            {
+                return children;
+            }
+
+            public T getData()
+            {
+                return data;
+            }
+
+            public INode<T> getParent()
+            {
+                return parent;
+            }
+
+            public void setParent(INode<T> parent)
+            {
+                this.parent = parent;
+            }
+
+            public bool addChild(INode<T> child)
+            {
+                if (getParents().Contains(child))
+                    return false;
+
+                children.Add(child);
+                child.setParent(this);
+
+                return true;
+            }
+
+            public bool addChild(T data)
+            {
+                Node node = new Node(data);
+                return addChild(node);
+            }
+
+            public void addChildren(List<INode<T>> chidlrenIN)
+            {
+                foreach (Node node in chidlrenIN)
+                {
+                    addChild(node);
+                }
+            }
+
+            public List<INode<T>> getParents()
+            {
+                List<INode<T>> parents = new List<INode<T>>();
+                INode<T> curr = this;
+
+                while (curr.getParent() != null)
+                {
+                    parents.Add(curr.getParent());
+                    curr = curr.getParent();
+                }
+
+                return parents;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj.GetType() == typeof(Node))
+                    return this.Equals(obj);
+
+                return false;
+            }
+
+            public bool Equals(INode<T> other)
+            {
+                return this.id == other.getID();
+            }
+
+            public int getID()
+            {
+                return this.id;
+            }
+        }
+    }
+}
