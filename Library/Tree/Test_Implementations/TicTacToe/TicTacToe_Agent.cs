a85836cb9a11351a67e772bbccd0d60a6b523991
Oliver Marie <oliver.j.a.marie@gmail.com>
2021-12-08 16:48:15 +0100

    TicTacToe Implementation

---
+++ Library/Tree/Test_Implementations/TicTacToe/TicTacToe_Agent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace C_Sharp_Lib.Library.Tree.Test_Implementations.TicTacToe
+{
+    public abstract class TicTacToe_Agent
+    {
+        protected TicTacToe game;
+
+        public TicTacToe_Agent(TicTacToe ticTacToe)
+        {
+            game = ticTacToe;
+        }
+
+        public abstract int play();
+    }
+}
