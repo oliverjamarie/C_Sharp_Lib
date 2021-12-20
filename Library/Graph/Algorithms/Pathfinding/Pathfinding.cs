9e95c5eec4f2acea21c06a2eb4c961294d231e75
Oliver Marie <oliver.j.a.marie@gmail.com>
2021-12-20 14:25:28 +0100

    Create Pathfinding.cs

---
+++ Library/Graph/Algorithms/Pathfinding/Pathfinding.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Library.Graph.Algorithms.Pathfinding
+{
+    public abstract class Pathfinding<T> where T : IComparable
+    {
+        protected Graph<T> graph;
+
+        public Pathfinding(Graph<T> graph)
+        {
+            this.graph = new Graph<T>();
+        }
+
+        public abstract List<INode<T>> FindShortestPath(INode<T> start, INode<T> end);
+        public abstract List<INode<T>> FindShortestPath(T start, T end);
+
+        public abstract double ShortestDistance(INode<T> start, INode<T> end);
+        public abstract double ShortestDistance(T start, T end);
+    }
+}
