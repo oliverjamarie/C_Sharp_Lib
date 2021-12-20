20c121c4d86719328142b0b09d86b32e02310a79
Oliver Marie <oliver.j.a.marie@gmail.com>
2021-12-20 14:25:47 +0100

    Create IMiniMax.cs

---
+++ Library/Tree/IMiniMax.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Library.Tree
+{
+    public interface IMiniMax<Move> : ITreeNode<Move>
+    {
+        enum MiniMaxAlgoMode
+        {
+            DEFAULT,
+            ALPHA_BETA_PRUNE
+        }
+
+        enum MiniMax
+        {
+            MAX_NODE,
+            MIN_NODE,
+            UNDEF
+        }
+
+        MiniMax NodeType
+        {
+            get;
+        }
+
+        MiniMaxAlgoMode AlgoMode
+        {
+            get;
+            set;
+        }
+        
+        // "new" used overidde parent interface
+        new IMiniMax<Move> Parent
+        {
+            get;
+            set;
+        }
+
+        int generateScore();
+    }
+}
