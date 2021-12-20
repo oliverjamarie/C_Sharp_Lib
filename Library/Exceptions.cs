48dcb2fada2392d9a77f9e374a38aec806bb617c
Oliver Marie <oliver.j.a.marie@gmail.com>
2021-12-20 14:24:41 +0100

    Create Exception.s

---
+++ Library/Exceptions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Library.Graph
+{
+    public class ElementNotInGraphException : Exception
+    {
+        public ElementNotInGraphException() : base("Element Not Found In Graph")
+        {
+        }
+    } 
+}
+
+namespace Library.Graph.Algorithms
+{
+    public class PrefAttachNullRootException : Exception
+    {
+        public PrefAttachNullRootException() :
+            base("Cannot start PrefAttach if graph's root node is null")
+        {
+
+        }
+    }
+
+    public class PrefAttachInvalidParamException : Exception
+    {
+        public PrefAttachInvalidParamException() : base("Sum of costs must be between 0 and 1")
+        {
+
+        }
+    }
+
+    public class InvalidTunerParameterException : Exception
+    {
+        public InvalidTunerParameterException() : base("Input must be between 0.0 and 1.0")
+        {
+
+        }
+    }
+}
